26794eb0fb7b0ab57bc33d0239f0428c3d83ed84
Phillip <philliplee515@gmail.com>
2022-04-05 14:54:05 +0900

    nest controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,10 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
+  @Get()
+  getAll() {
+    return 'this will return all movies';
+  }
 
-    @Get()
-    getAll(){
-        return "this will return all movies"
-    }
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return `This will create a movie`;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `this will delete a move with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `this will patch a movie with the id: ${movieId}`;
+  }
 }
